14037ff4123215eb747106fe1cc374e8de83bc36
admin <duchieupham1709@gmail.com>
2024-02-05 14:06:32 +0700

    implementing bidv invoice management

---
+++ src/main/java/com/vietqr/org/controller/bidv/BillPaymentController.java
@@ -0,0 +1,17 @@
+package com.vietqr.org.controller.bidv;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class BillPaymentController {
+    private static final Logger logger = Logger.getLogger(BillPaymentController.class);
+
+    // @PostMapping("bidv/getbill")
+
+}
---
+++ src/main/java/com/vietqr/org/entity/bidv/CustomerInvoiceEntity.java
@@ -0,0 +1,25 @@
+package com.vietqr.org.entity.bidv;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CustomerInvoice")
+public class CustomerInvoiceEntity implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "customer_id")
+    private String customerId;
+
+}
