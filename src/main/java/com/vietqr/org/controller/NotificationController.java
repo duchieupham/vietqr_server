7f74d2fe81723f75fefc61fc5cec637a73ccbdac
Linhsii <linh.npn@vietqr.vn>
2024-03-01 15:31:32 +0700

    Disable Notification and Contact

--- src/main/java/com/vietqr/org/controller/NotificationController.java
+++ src/main/java/com/vietqr/org/controller/NotificationController.java
@@ -37,44 +37,45 @@ public class NotificationController {
     public ResponseEntity<List<NotificationEntity>> getNotificationsByUserId(
             @Valid @RequestBody NotificationInputDTO dto) {
         List<NotificationEntity> result = new ArrayList<>();
-        HttpStatus httpStatus = null;
-        try {
-            result = notificationService.getNotificationsByUserId(dto.getUserId(), dto.getOffset());
-            httpStatus = HttpStatus.OK;
-        } catch (Exception e) {
-            logger.error("Notification Error: " + e.toString());
-            httpStatus = HttpStatus.BAD_REQUEST;
-        }
+        HttpStatus httpStatus = HttpStatus.OK;
+//        HttpStatus httpStatus = null;
+//        try {
+//            result = notificationService.getNotificationsByUserId(dto.getUserId(), dto.getOffset());
+//            httpStatus = HttpStatus.OK;
+//        } catch (Exception e) {
+//            logger.error("Notification Error: " + e.toString());
+//            httpStatus = HttpStatus.BAD_REQUEST;
+//        }
         return new ResponseEntity<>(result, httpStatus);
     }
 
     @GetMapping("notification/count/{userId}")
     ResponseEntity<NotificationCountDTO> getNotificationCountByUserId(@PathVariable(value = "userId") String userId) {
-        NotificationCountDTO result = null;
-        HttpStatus httpStatus = null;
-        try {
-            int count = notificationService.getNotificationCountByUserId(userId);
-            result = new NotificationCountDTO(count);
-            httpStatus = HttpStatus.OK;
-        } catch (Exception e) {
-            logger.error("Notification Count Error: " + e.toString());
-            httpStatus = HttpStatus.BAD_REQUEST;
-        }
+        NotificationCountDTO result = new NotificationCountDTO(0);
+        HttpStatus httpStatus = HttpStatus.OK;
+//        try {
+//            int count = notificationService.getNotificationCountByUserId(userId);
+//            result = new NotificationCountDTO(count);
+//            httpStatus = HttpStatus.OK;
+//        } catch (Exception e) {
+//            logger.error("Notification Count Error: " + e.toString());
+//            httpStatus = HttpStatus.BAD_REQUEST;
+//        }
         return new ResponseEntity<>(result, httpStatus);
     }
 
     @PostMapping("notification/status")
     ResponseEntity<ResponseMessageDTO> updateNotificationStatus(@Valid @RequestBody NotificationStatusDTO dto) {
         ResponseMessageDTO result = null;
-        HttpStatus httpStatus = null;
-        try {
-            notificationService.updateNotificationStatus(dto.getUserId());
-            httpStatus = HttpStatus.OK;
-        } catch (Exception e) {
-            logger.error("Notification Count Error: " + e.toString());
-            result = new ResponseMessageDTO("FAILED", "E05");
-            httpStatus = HttpStatus.BAD_REQUEST;
-        }
+        HttpStatus httpStatus = HttpStatus.OK;
+//        try {
+//            notificationService.updateNotificationStatus(dto.getUserId());
+//            httpStatus = HttpStatus.OK;
+//        } catch (Exception e) {
+//            logger.error("Notification Count Error: " + e.toString());
+//            result = new ResponseMessageDTO("FAILED", "E05");
+//            httpStatus = HttpStatus.BAD_REQUEST;
+//        }
         return new ResponseEntity<>(result, httpStatus);
     }
 
