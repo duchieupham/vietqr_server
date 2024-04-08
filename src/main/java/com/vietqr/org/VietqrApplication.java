8f60c2cdc2dcc3aee924513d1b4d4fb00fe3bb71
admin <duchieupham1709@gmail.com>
2024-04-08 12:03:43 +0700

    implementing bidv update

--- src/main/java/com/vietqr/org/VietqrApplication.java
+++ src/main/java/com/vietqr/org/VietqrApplication.java
@@ -162,7 +162,7 @@ public class VietqrApplication extends SpringBootServletInitializer implements W
 		// "FT23293978692076", "SABAccessKey");
 		// System.out.println("CHECKSUM REFUND: " + checkSum2);
 
-		String bankAccountEncrypted = BankEncryptUtil.encrypt("6035105698389");
+		String bankAccountEncrypted = BankEncryptUtil.encrypt("140677");
 		System.out.println("bankAccountEncrypted: " + bankAccountEncrypted);
 
 		/// generate signature to request payment MB
