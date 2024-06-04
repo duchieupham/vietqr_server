170f4f610f1a8a46861a6f48ce647fde8a267d1b
Linhsii <linh.npn@vietqr.vn>
2024-06-05 00:41:38 +0700

    Fix config application

--- src/main/java/com/vietqr/org/util/EnvironmentUtil.java
+++ src/main/java/com/vietqr/org/util/EnvironmentUtil.java
@@ -217,7 +217,7 @@ public class EnvironmentUtil {
     private static final String BANK_NAME_RECHARGE_UAT = "Ngân hàng TMCP Quân đội";
     private static final String BANK_NAME_RECHARGE_PROD = "Ngân hàng TMCP Quân đội";
     //
-    private static final String USER_BANK_NAME_RECHARGE_UAT = "Nguyen Phuong Nhat Linh";
+    private static final String USER_BANK_NAME_RECHARGE_UAT = "Bluecom Viet Nam";
     private static final String USER_BANK_NAME_RECHARGE_UAT2 = "Cong Ty Co Phan Bluecom Viet Nam";
     private static final String USER_BANK_NAME_RECHARGE_PROD = "Pham Duc Tuan";
     private static final String USER_BANK_NAME_RECHARGE_PROD2 = "Cong Ty Co Phan Bluecom Viet Nam";
