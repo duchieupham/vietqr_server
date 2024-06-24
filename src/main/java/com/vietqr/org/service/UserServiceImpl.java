e15971d0c25cb25ee3c8604f4585a3c2cd4301a3
quang-van <van.pq@vietqr.vn>
2024-06-24 14:16:58 +0700

    fix/synchronized numbers user-in-date and all-user by grpc

--- src/main/java/com/vietqr/org/service/UserServiceImpl.java
+++ src/main/java/com/vietqr/org/service/UserServiceImpl.java
@@ -2,13 +2,13 @@ package com.vietqr.org.service;
 
 import com.example.grpc.GetUsersRequest;
 import com.example.grpc.GetUsersResponse;
-import com.example.grpc.UserServiceGrpc;
 import com.example.grpc.User;
+import com.example.grpc.UserServiceGrpc;
 import com.vietqr.org.dto.IAccountLogin;
-
 import io.grpc.stub.StreamObserver;
-import org.springframework.beans.factory.annotation.Autowired;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -28,8 +28,20 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
         long endTime = date.plusDays(1).atStartOfDay().toEpochSecond(ZoneOffset.UTC);
 
         System.out.println("Received request for date: " + date);
+
+        // Số người dùng đăng ký trong ngày
         List<IAccountLogin> users = accountLoginService.findUsersRegisteredInDay(startTime, endTime);
+        long userRegister = users.size();
+
+        // Tổng số người dùng
+        long totalUsers = accountLoginService.getTotalUsers();
+
+        // Tạo JSON string cho sumUser
+        String sumUserJson = String.format("{\"user_register\": \"%d\", \"total\":\"%d\"}", userRegister, totalUsers);
+
+        // Xây dựng phản hồi gRPC
         GetUsersResponse.Builder responseBuilder = GetUsersResponse.newBuilder();
+        responseBuilder.setSumUser(sumUserJson);
 
         for (IAccountLogin user : users) {
             User.Builder grpcUserBuilder = User.newBuilder().setId(user.getId());
@@ -46,6 +58,7 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
 
             responseBuilder.addUsers(grpcUserBuilder.build());
         }
+
         System.out.println("Sending response with " + users.size() + " users");
 
         responseObserver.onNext(responseBuilder.build());
