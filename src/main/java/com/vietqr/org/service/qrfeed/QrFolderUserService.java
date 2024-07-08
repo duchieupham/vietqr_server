86ba521164137a3ff07c1127ad2d86dec9614598
quocanhit58 <anh.ttq@vietqr.vn>
2024-07-08 15:44:06 +0700

    improve add user admin in create folder

--- src/main/java/com/vietqr/org/service/qrfeed/QrFolderUserService.java
+++ src/main/java/com/vietqr/org/service/qrfeed/QrFolderUserService.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface QrFolderUserService {
 
     void addUserIds(String qrFolderId, List<UserRoleDTO> userRoles, String userId);
+    void addUserAdmin(String qrFolderId, String folderId, String userId);
 
     public List<IUserInFolderDTO> getUserInFolder(String qrFolderId);
 
