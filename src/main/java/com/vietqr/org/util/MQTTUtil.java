30eb8b3910cee22b1a25f7ff0eff729d40da1ec0
Linhsii <linh.npn@vietqr.vn>
2024-08-13 00:18:28 +0700

    Update mqtt util

--- src/main/java/com/vietqr/org/util/MQTTUtil.java
+++ src/main/java/com/vietqr/org/util/MQTTUtil.java
@@ -1,19 +1,25 @@
-//package com.vietqr.org.util;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class MQTTUtil {
-//
-//    private static MQTTHandler mqttHandler;
-//
-//    @Autowired
-//    public MQTTUtil(MQTTHandler mqttHandler) {
-//        MQTTUtil.mqttHandler = mqttHandler;
-//    }
-//
-//    public static void sendMessage(String topic, String messageContent) {
-//        mqttHandler.sendMessage(topic, messageContent);
-//    }
-//}
+package com.vietqr.org.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MQTTUtil {
+    private static final Logger logger = Logger.getLogger(MQTTUtil.class);
+
+    private static MqttListenerService mqttHandler;
+
+    @Autowired
+    public MQTTUtil(MqttListenerService mqttHandler) {
+        MQTTUtil.mqttHandler = mqttHandler;
+    }
+
+    public static void sendMessage(String topic, String messageContent) {
+        try {
+            mqttHandler.publishMessageToCommonTopic(topic, messageContent);
+        } catch (Exception e) {
+            logger.error("MQTTUtil: ERROR: " + e.getMessage() + " at: " + System.currentTimeMillis());
+        }
+    }
+}
