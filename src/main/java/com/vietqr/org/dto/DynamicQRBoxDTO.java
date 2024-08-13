b1f4da35b33bbdcc73a1a94abcf66981259b5638
Linhsii <linh.npn@vietqr.vn>
2024-08-14 00:19:58 +0700

    Improve mqtt

--- src/main/java/com/vietqr/org/dto/DynamicQRBoxDTO.java
+++ src/main/java/com/vietqr/org/dto/DynamicQRBoxDTO.java
@@ -2,29 +2,15 @@ package com.vietqr.org.dto;
 
 public class DynamicQRBoxDTO {
     private String notificationType;
-    private String transactionReceiveId;
-    private String bankAccount;
-    private String bankShortName;
-    private String userBankName;
-    private String content;
     private String amount;
     private String qrCode;
-    private String qrType;
-
     public DynamicQRBoxDTO() {
     }
 
-    public DynamicQRBoxDTO(String notificationType, String transactionReceiveId, String bankAccount, String bankShortName,
-                           String userBankName, String content, String amount, String qrCode, String qrType) {
+    public DynamicQRBoxDTO(String notificationType, String amount, String qrCode) {
         this.notificationType = notificationType;
-        this.transactionReceiveId = transactionReceiveId;
-        this.bankAccount = bankAccount;
-        this.bankShortName = bankShortName;
-        this.userBankName = userBankName;
-        this.content = content;
         this.amount = amount;
         this.qrCode = qrCode;
-        this.qrType = qrType;
     }
 
     public String getNotificationType() {
@@ -33,46 +19,6 @@ public class DynamicQRBoxDTO {
 
     public void setNotificationType(String notificationType) {
         this.notificationType = notificationType;
-    }
-
-    public String getTransactionReceiveId() {
-        return transactionReceiveId;
-    }
-
-    public void setTransactionReceiveId(String transactionReceiveId) {
-        this.transactionReceiveId = transactionReceiveId;
-    }
-
-    public String getBankAccount() {
-        return bankAccount;
-    }
-
-    public void setBankAccount(String bankAccount) {
-        this.bankAccount = bankAccount;
-    }
-
-    public String getBankShortName() {
-        return bankShortName;
-    }
-
-    public void setBankShortName(String bankShortName) {
-        this.bankShortName = bankShortName;
-    }
-
-    public String getUserBankName() {
-        return userBankName;
-    }
-
-    public void setUserBankName(String userBankName) {
-        this.userBankName = userBankName;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
     public String getAmount() {
@@ -89,13 +35,5 @@ public class DynamicQRBoxDTO {
 
     public void setQrCode(String qrCode) {
         this.qrCode = qrCode;
-    }
-
-    public String getQrType() {
-        return qrType;
-    }
-
-    public void setQrType(String qrType) {
-        this.qrType = qrType;
     }
 }
