9cacb325c912cd846616c7f6c56bd5b8fba6704f
quang-van <van.pq@vietqr.vn>
2024-08-22 15:43:32 +0700

    feat/done DS001

--- src/main/java/com/vietqr/org/dto/AdminInvoiceDTO.java
+++ src/main/java/com/vietqr/org/dto/AdminInvoiceDTO.java
@@ -1,5 +1,8 @@
 package com.vietqr.org.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AdminInvoiceDTO {
     private String invoiceId;
     private long timePaid;
--- src/main/java/com/vietqr/org/dto/DataDTO.java
+++ src/main/java/com/vietqr/org/dto/DataDTO.java
@@ -1,8 +1,10 @@
 package com.vietqr.org.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DataDTO {
     private List<?> items;
     private Object extraData;
