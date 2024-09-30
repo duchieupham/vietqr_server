ba3d0e3db42ee4114b84a43f92276838f2d3ad4c
Linhsii <linh.npn@vietqr.vn>
2024-09-30 17:45:46 +0000

    Comment method not using

--- src/main/java/com/vietqr/org/service/grpc/biz/BankAccountReceiveServer.java
+++ src/main/java/com/vietqr/org/service/grpc/biz/BankAccountReceiveServer.java
@@ -25,74 +25,74 @@ public class BankAccountReceiveServer extends BankAccountReceiveServiceGrpc.Bank
 
     @Override
     public void getBankAccountReceiveByBankId(RequestByBankId request, StreamObserver<BankAccountReceive> responseObserver) {
-        try {
-            IBankAccountReceiveUserDTO result = accountBankReceiveRepository.getBankAccountReceiveByBankIdGrpc(request.getBankId().trim());
-            BankAccountReceive response = BankAccountReceive
-                    .newBuilder()
-                    .setId(request.getBankId().trim())
-                    .setBankAccount(result.getBankAccount())
-                    .setUserBankName(result.getUserBankName())
-                    .setIsSync(result.getIsSync())
-                    .setBankTypeId(result.getBankTypeId())
-                    .setUserId(result.getUserId())
-                    .setBankShortName(result.getBankShortName())
-                    .build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            logger.error(LOG_ERROR + "getBankAccountReceiveByBankId: " + e.getMessage() + " at: " + System.currentTimeMillis());
-            BankAccountReceive response = BankAccountReceive
-                    .newBuilder()
-                    .setId(request.getBankId().trim())
-                    .setBankAccount("")
-                    .setUserBankName("")
-                    .setIsSync(false)
-                    .setBankTypeId("")
-                    .setUserId("")
-                    .setBankShortName("")
-                    .build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        }
+//        try {
+//            IBankAccountReceiveUserDTO result = accountBankReceiveRepository.getBankAccountReceiveByBankIdGrpc(request.getBankId().trim());
+//            BankAccountReceive response = BankAccountReceive
+//                    .newBuilder()
+//                    .setId(request.getBankId().trim())
+//                    .setBankAccount(result.getBankAccount())
+//                    .setUserBankName(result.getUserBankName())
+//                    .setIsSync(result.getIsSync())
+//                    .setBankTypeId(result.getBankTypeId())
+//                    .setUserId(result.getUserId())
+//                    .setBankShortName(result.getBankShortName())
+//                    .build();
+//            responseObserver.onNext(response);
+//            responseObserver.onCompleted();
+//        } catch (Exception e) {
+//            logger.error(LOG_ERROR + "getBankAccountReceiveByBankId: " + e.getMessage() + " at: " + System.currentTimeMillis());
+//            BankAccountReceive response = BankAccountReceive
+//                    .newBuilder()
+//                    .setId(request.getBankId().trim())
+//                    .setBankAccount("")
+//                    .setUserBankName("")
+//                    .setIsSync(false)
+//                    .setBankTypeId("")
+//                    .setUserId("")
+//                    .setBankShortName("")
+//                    .build();
+//            responseObserver.onNext(response);
+//            responseObserver.onCompleted();
+//        }
     }
 
     @Override
     public void getBankAccountReceiveByUserId(RequestByUserId request, StreamObserver<BankAccountReceiveList> responseObserver) {
-        try {
-            List<IBankAccountReceiveBankDTO> result = accountBankReceiveRepository.getBankAccountReceiveByUserIdGrpc(request.getUserId().trim());
-            BankAccountReceiveList.Builder response = BankAccountReceiveList.newBuilder();
-            for (IBankAccountReceiveBankDTO item : result) {
-                BankAccountReceive bankAccountReceive = BankAccountReceive
-                        .newBuilder()
-                        .setId(item.getBankId())
-                        .setBankAccount(item.getBankAccount())
-                        .setUserBankName(item.getUserBankName())
-                        .setIsSync(item.getIsSync())
-                        .setBankTypeId(item.getBankTypeId())
-                        .setUserId(request.getUserId().trim())
-                        .setBankShortName(item.getBankShortName())
-                        .build();
-                response.addBankAccountReceives(bankAccountReceive);
-            }
-            responseObserver.onNext(response.build());
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            logger.error(LOG_ERROR + "getBankAccountReceiveByUserId: " + e.getMessage() + " at: " + System.currentTimeMillis());
-            BankAccountReceiveList response = BankAccountReceiveList
-                    .newBuilder()
-                    .addBankAccountReceives(
-                            BankAccountReceive
-                                    .newBuilder()
-                                    .setBankAccount("")
-                                    .setUserBankName("")
-                                    .setIsSync(false)
-                                    .setBankTypeId("")
-                                    .setUserId("")
-                                    .setBankShortName("")
-                                    .build()
-                    ).build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        }
+//        try {
+//            List<IBankAccountReceiveBankDTO> result = accountBankReceiveRepository.getBankAccountReceiveByUserIdGrpc(request.getUserId().trim());
+//            BankAccountReceiveList.Builder response = BankAccountReceiveList.newBuilder();
+//            for (IBankAccountReceiveBankDTO item : result) {
+//                BankAccountReceive bankAccountReceive = BankAccountReceive
+//                        .newBuilder()
+//                        .setId(item.getBankId())
+//                        .setBankAccount(item.getBankAccount())
+//                        .setUserBankName(item.getUserBankName())
+//                        .setIsSync(item.getIsSync())
+//                        .setBankTypeId(item.getBankTypeId())
+//                        .setUserId(request.getUserId().trim())
+//                        .setBankShortName(item.getBankShortName())
+//                        .build();
+//                response.addBankAccountReceives(bankAccountReceive);
+//            }
+//            responseObserver.onNext(response.build());
+//            responseObserver.onCompleted();
+//        } catch (Exception e) {
+//            logger.error(LOG_ERROR + "getBankAccountReceiveByUserId: " + e.getMessage() + " at: " + System.currentTimeMillis());
+//            BankAccountReceiveList response = BankAccountReceiveList
+//                    .newBuilder()
+//                    .addBankAccountReceives(
+//                            BankAccountReceive
+//                                    .newBuilder()
+//                                    .setBankAccount("")
+//                                    .setUserBankName("")
+//                                    .setIsSync(false)
+//                                    .setBankTypeId("")
+//                                    .setUserId("")
+//                                    .setBankShortName("")
+//                                    .build()
+//                    ).build();
+//            responseObserver.onNext(response);
+//            responseObserver.onCompleted();
+//        }
     }
 }
